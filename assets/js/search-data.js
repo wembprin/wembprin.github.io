9b4b763381d0d7e4dcf1be82a843cfa0bd1c0c30
andreapdr <32898447+andreapdr@users.noreply.github.com>
2026-01-08 14:18:50 +0000

    Deploying to gh-pages from @ wembprin/wembprin.github.io@cc36b5972cef755f24c7c6add737d0c3673d83a9 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,148 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-home",
+    title: "home",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-event",
+          title: "event",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/event/";
+          },
+        },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather/";
+            },},{id: "news-desegma-it-has-been-accepted-as-a-shared-task-of-evalita-2026",
+          title: 'DeSegMA-IT has been accepted as a Shared Task of EVALITA 2026',
+          description: "",
+          section: "News",},{id: "news-subtask-a-data-release",
+          title: 'SubTask A Data Release',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/desegma_contd/";
+            },},{id: "news-registration-form",
+          title: 'Registration Form',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/desegma_register/";
+            },},{id: "news-subtask-b-data-release",
+          title: 'SubTask B Data Release',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/desegma_subtaskB/";
+            },},{id: "organizers-01-fsebastiani",
+          title: '01 Fsebastiani',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/01-fsebastiani/";
+            },},{id: "organizers-02-mbolognesi",
+          title: '02 Mbolognesi',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/02-mbolognesi/";
+            },},{id: "organizers-03-gsperduti",
+          title: '03 Gsperduti',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/03-gsperduti/";
+            },},{id: "organizers-04-apedrotti",
+          title: '04 Apedrotti',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/04-apedrotti/";
+            },},{id: "organizers-05-gpucce",
+          title: '05 Gpucce',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/05-gpucce/";
+            },},{id: "organizers-06-grambelli",
+          title: '06 Grambelli',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/06-grambelli/";
+            },},{id: "organizers-aesuli",
+          title: 'Aesuli',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/aesuli/";
+            },},{id: "organizers-apedrotti",
+          title: 'Apedrotti',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/apedrotti/";
+            },},{id: "organizers-gpucce",
+          title: 'Gpucce',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/gpucce/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-inspire',
+        title: 'Inspire HEP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.alberteinstein.com/", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
